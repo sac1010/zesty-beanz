51d1fe02ae91b5f1afb7e4a87d2e0eba8cf870da
sac1010 <sachingirish101@gmail.com>
2024-05-17 14:41:46 +0530

    responsiveness

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   
   return (
     <div className="flex items-center justify-center w-full md:h-screen">
-        <div className='bg-[#9AC8CD] min-h-screen md:min-h-fit w-full md:w-8/12 md:px-16 md:rounded-3xl text-center flex flex-col justify-between'>
+        <div className='bg-[#9AC8CD] min-h-screen md:min-h-fit w-full md:w-8/12 2xl:w-6/12 md:px-16 md:rounded-3xl text-center flex flex-col justify-between'>
           <div className='my-7 text-3xl h-full font-bold text-[#1E0342]'>Weather</div>
           <SearchWeather onWeatherData={handleData}/>
           <WeatherInfo key={weatherData?.name} data={weatherData}/>
